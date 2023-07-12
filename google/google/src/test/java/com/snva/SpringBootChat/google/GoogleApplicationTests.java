bfd74c6c2b02d0155b0432c838171847258929f7
prabhjot <123002355+deedarka@users.noreply.github.com>
2023-07-12 10:21:53 -0400

    Add files via upload

---
+++ google/google/src/test/java/com/snva/SpringBootChat/google/GoogleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.snva.SpringBootChat.google;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoogleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
